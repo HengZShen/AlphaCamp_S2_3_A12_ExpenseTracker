66b1e1b7727bd4632a1573b1a1875e08a3421622
HengZShen <r06621112@gmail.com>
2021-03-30 17:13:29 +0800

    feat: set handlebars, static, body-parser, method override

--- app.js
+++ app.js
@@ -3,14 +3,32 @@ const PORT = 3000
 const app = express()
 
 
+// static files
+app.use(express.static('public'))
+
+// handlebars
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
+// method override 
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
+
+// mongodb connection
+require('./configs/mongoose')
 
 
 
 app.get('/', (req, res) => {
-  res.send('12345')
+  res.render('index')
 })
 
 
