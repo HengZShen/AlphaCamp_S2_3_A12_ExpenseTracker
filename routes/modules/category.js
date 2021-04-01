6c7fc5478e41fbd1927606fb1b4b9678547f911b
HengZShen <r06621112@gmail.com>
2021-04-01 12:00:04 +0800

    feat: add all category

--- routes/modules/category.js
+++ routes/modules/category.js
@@ -5,17 +5,15 @@ const Expense = require('../../models/Record')
 
 router.get('/', (req, res) => {
 
+  if (req.query.category === 'all') {
+    req.query = {}
+  }
+
   Expense.find(req.query)
     .lean()
     .then(expenses => res.render('index', { expenses }))
     .catch(error => console.log(error))
 })
-
-
-
-
-
-
 
 
 
